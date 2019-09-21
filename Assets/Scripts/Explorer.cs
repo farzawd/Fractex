ae30a3e3732a614b6c02214fb05b79ae343cb743
Farzad Gholizadeh <farzad.gholizadeh@gmail.com>
2019-09-21 22:13:32 +0430

    add fractal explorer script

---
+++ Assets/Scripts/Explorer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explorer : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    
+    [SerializeField] private Vector2 position;
+    [SerializeField] private float scale;
+
+    void Update()
+    {
+        float ar = (float) Screen.width / Screen.height;
+
+        float scaleX = scale;
+        float scaleY = scale;
+
+        if (ar > 1f)
+        {
+            scaleY /= ar;
+        }
+        else
+        {
+            scaleX *= ar;
+        }
+        
+        material.SetVector("_Area", new Vector4(position.x, position.y, scaleX, scaleY));
+    }
+}
